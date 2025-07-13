d8b08d064b527446b99d17279a21377133812ba2
Jan Vollmer <jan@vllmr.dev>
2025-07-13 15:06:04 +0200

    feat: first implementation of cron tasks
    Signed-off-by: Jan Vollmer <jan@vllmr.dev>

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/jvllmr/frans/pkg/config"
+	fransCron "github.com/jvllmr/frans/pkg/cron"
 	logging "github.com/jvllmr/frans/pkg/logger"
 	apiRoutes "github.com/jvllmr/frans/pkg/routes/api"
 	clientRoutes "github.com/jvllmr/frans/pkg/routes/client"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	slogmulti "github.com/samber/slog-multi"
+	"github.com/spf13/cobra"
+
+	"github.com/robfig/cron/v3"
 	"go.opentelemetry.io/contrib/bridges/otelslog"
 )
 
@@ -38,15 +42,29 @@ func setupRouter(configValue config.Config) *gin.Engine {
 	return r
 }
 
-func main() {
-	godotenv.Load()
+func getSafeConfig() config.Config {
 	configValue, err := config.GetConfig()
 	if err != nil {
 		panic(err)
 	}
+	return configValue
+}
+
+func initFrans(configValue config.Config) {
 
 	config.InitDB(configValue)
-	defer config.DBClient.Close()
+	var logHandler slog.Handler = slog.NewTextHandler(os.Stdout, nil)
+	if configValue.LogJSON {
+		logHandler = slog.NewJSONHandler(os.Stdout, nil)
+	}
+
+	basicLogger := slog.New(logHandler)
+	slog.SetDefault(basicLogger)
+
+}
+
+func startGin() {
+	configValue := getSafeConfig()
 	config.InitOIDC(configValue)
 	if configValue.DevMode {
 		slog.Info("frans was started in development mode")
@@ -57,5 +75,77 @@ func main() {
 	serveString := fmt.Sprintf("%s:%d", configValue.Host, configValue.Port)
 	slog.Info(fmt.Sprintf("Serving on %s", serveString))
 	r.Run(serveString)
+}
 
+func startCronScheduler() {
+	configValue := getSafeConfig()
+	cronRunner := cron.New()
+	cronRunner.AddFunc("@every 1m", fransCron.SessionLifecycleTask)
+	cronRunner.AddFunc("@every 1m", func() {
+		fransCron.FileLifecycleTask(configValue)
+	})
+
+	cronRunner.AddFunc("@every 1m", func() {
+		fransCron.TicketsLifecycleTask(configValue)
+	})
+
+	cronRunner.Run()
+
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "frans",
+	Short: "A simple file-sharing tool ready for cloud native",
+	Run: func(cmd *cobra.Command, args []string) {
+		go startCronScheduler()
+		startGin()
+	},
+}
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start only the server",
+	Run:   func(cmd *cobra.Command, args []string) { startGin() },
+}
+
+var cronCmd = &cobra.Command{
+	Use:   "cron",
+	Short: "Start only the cron scheduler",
+	Run:   func(cmd *cobra.Command, args []string) { startCronScheduler() },
+}
+
+var taskCommand = &cobra.Command{
+	Use:   "task",
+	Short: "Start specific cron task once",
+}
+
+var sessionLifecycleTaskCommand = &cobra.Command{
+	Use:   "lifecycle-session",
+	Short: "Delete stale sessions",
+}
+
+var ticketLifecycleTaskCommand = &cobra.Command{
+	Use:   "lifecycle-ticket",
+	Short: "Delete stale tickets",
+}
+
+var fileLifecycleTaskCommand = &cobra.Command{
+	Use:   "lifecycle-file",
+	Short: "Delete stale files",
+}
+
+func main() {
+	godotenv.Load()
+	configValue := getSafeConfig()
+	initFrans(configValue)
+	defer config.DBClient.Close()
+
+	taskCommand.AddCommand(
+		sessionLifecycleTaskCommand,
+		ticketLifecycleTaskCommand,
+		fileLifecycleTaskCommand,
+	)
+	rootCmd.AddCommand(taskCommand, cronCmd, serveCmd)
+
+	cobra.CheckErr(rootCmd.Execute())
 }
---
+++ pkg/cron/file.go
@@ -0,0 +1,40 @@
+package fransCron
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/jvllmr/frans/pkg/config"
+	"github.com/jvllmr/frans/pkg/ent/file"
+	"github.com/jvllmr/frans/pkg/util"
+)
+
+func FileLifecycleTask(configValue config.Config) {
+	files := config.DBClient.File.Query().
+		Where(file.TimesDownloadedGT(0)).
+		WithTickets().
+		AllX(context.Background())
+	deletedCount := 0
+	for _, fileValue := range files {
+		skip := false
+		for _, ticketValue := range fileValue.Edges.Tickets {
+			if ticketValue.ExpiryType == config.TicketExpiryTypeNone {
+				continue
+			}
+			if ticketValue.ExpiryType == config.TicketExpiryTypeCustom &&
+				ticketValue.ExpiryTotalDownloads > uint8(fileValue.TimesDownloaded) ||
+				ticketValue.ExpiryType == config.TicketExpiryTypeAuto &&
+					configValue.DefaultExpiryTotalDownloads > uint8(fileValue.TimesDownloaded) {
+				skip = true
+			}
+		}
+		if !skip {
+			filePath := util.GetFilesFilePath(configValue, fileValue.Sha512)
+			_ = os.Remove(filePath)
+			config.DBClient.File.DeleteOne(fileValue).ExecX(context.Background())
+			deletedCount += 1
+		}
+	}
+	slog.Info("Deleted files", "count", deletedCount)
+}
