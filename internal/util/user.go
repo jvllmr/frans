fb34fbe3a9df3dff6c3c721cc620dafe8efe3500
Jan Vollmer <jan@vllmr.dev>
2025-09-06 16:32:10 +0200

    fix(server): avoid transaction lock errors
    Signed-off-by: Jan Vollmer <jan@vllmr.dev>

--- internal/util/user.go
+++ internal/util/user.go
@@ -7,13 +7,17 @@ import (
 	"github.com/jvllmr/frans/internal/ent/file"
 )
 
-func RefreshUserTotalDataSize(ctx context.Context, userValue *ent.User) error {
+func RefreshUserTotalDataSize(ctx context.Context, userValue *ent.User, tx *ent.Tx) error {
 	if totalDataSize, err := userValue.QueryTickets().
 		QueryFiles().
 		Aggregate(ent.Sum(file.FieldSize)).
 		Int(ctx); err != nil {
 		return err
 	} else {
+		if tx != nil {
+			tx.User.UpdateOne(userValue).SetTotalDataSize(int64(totalDataSize)).Exec(ctx)
+		}
+
 		return userValue.Update().SetTotalDataSize(int64(totalDataSize)).Exec(ctx)
 	}
 }
